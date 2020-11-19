601145848a94fcb7fc58ec6b8e5b32cf012a1c74
Guillaume <guillaume.grazioli@etud.univ-jfc.fr>
2020-11-19 10:30:07 +0100

    stateFinal

--- src/main/java/piechart/PercentagePieChart.java
+++ src/main/java/piechart/PercentagePieChart.java
@@ -7,88 +7,119 @@ import javax.swing.event.MouseInputAdapter;
 import javax.swing.event.MouseInputListener;
 
 /**
- * A PercentagePieChart acts boths as a MVC View and Controller of a Percentage It maintains a reference to its model in order to
- * update it.
+ * A PercentagePieChart acts boths as a MVC View and Controller of a Percentage
+ * It maintains a reference to its model in order to update it.
  *
  */
 public class PercentagePieChart extends JComponent implements PercentageView {
 
-	// Predefined cursors
+    // Predefined cursors
+    private static final Cursor HAND = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
+    private static final Cursor CROSS = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+    private static final Cursor ARROW = Cursor.getDefaultCursor();
 
-	private static final Cursor HAND = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
-	private static final Cursor CROSS = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
-	private static final Cursor ARROW = Cursor.getDefaultCursor();
+    /**
+     * Hold a reference to the model
+     */
+    private final PercentageModel myModel;
 
-	/**
-	 * Hold a reference to the model
-	 */
-	private final PercentageModel myModel;
-
-	// Implementation of the PieChart's StateChart
-	private enum PieState {
+    // Implementation of the PieChart's StateChart
+    /*private enum PieState {
 		INIT, IN_PIN, ADJUSTING
-	};
-	private PieState myState = PieState.INIT;
-
-	public PercentagePieChart(PercentageModel model) {
-		super();
-
-		// "Controller" behaviour : handle mouse input and update the percentage accordingly
-		MouseInputListener l = new MouseInputAdapter() {
-                    // State Objects
-    // 1) Pour chaque état on crée une classe interne anonyme qui étend State
-    // 2) On surcharge la ou les méthodes qu'on veut traiter
-    //            (ici mousePressed )
-    // 3) On crée une instance de cette classe anonyme
-                    private class AbstractState extends MouseInputAdapter {
-    };
-    private AbstractState IN_PIN = new AbstractState() {
-        @Override
-        public void mousePressed(MouseEvent e) {
-            currentState = ADJUSTING;
-            setCursor(CROSS);
-        }
-    };
-    private AbstractState ADJUSTING = new AbstractState() {
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            if (inPin(e)) {
-                    currentState = IN_PIN;
+	};*/
+    //private PieState myState = PieState.INIT;
+    private class AbstractState extends MouseInputAdapter {
+            };
+            private AbstractState InPin = new AbstractState() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    currentState = ADJUSTING;
                     setCursor(CROSS);
-            } else {
-                    currentState = INIT;
-                    setCursor(ARROW);
-            }
-        }
-        
-        // State variable
-        private AbstractState currentState = INIT;
+                    repaint();
+                }
+                @Override
+                public void mouseMoved(MouseEvent e ){
+                    if (!inPin(e)) {
+                            setCursor(ARROW);
+                            currentState = INIT;
+                    }
+                }
+            };
+            private AbstractState ADJUSTING = new AbstractState() {
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    if (inPin(e)) {
+                        currentState = InPin;
+                        setCursor(CROSS);
+                    } else {
+                        currentState = INIT;
+                        setCursor(ARROW);
+                    }
+                    repaint();
+                }
+                @Override
+                public void mouseDragged(MouseEvent e) {
+                   myModel.setValue(pointToPercentage(e));
+                   repaint();
+                }
+            };
 
-    // On délègue la gestion des événements à l'état courant
-    MouseInputAdapter eventHandler = new MouseInputAdapter() {
-        @Override
-        public void mousePressed(MouseEvent e) {
-            currentState.mousePressed(e);
-        }
+            private AbstractState INIT = new AbstractState() {
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    if (inPin(e)) {
+                        setCursor(HAND);
+                        currentState = InPin;
+                    }
+                }
 
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            currentState.mouseReleased(e);
-        }
+            }; 
+            // State variable
+            private AbstractState currentState = INIT;
+    public PercentagePieChart(PercentageModel model) {
+        super();
 
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            currentState.mouseDragged(e);
-        }
-    };
+        // "Controller" behaviour : handle mouse input and update the percentage accordingly
+        MouseInputListener l = new MouseInputAdapter() {
+            // State Objects
+            // 1) Pour chaque état on crée une classe interne anonyme qui étend State
+            // 2) On surcharge la ou les méthodes qu'on veut traiter
+            //            (ici mousePressed )
+            // 3) On crée une instance de cette classe anonyme
+            
 
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            moveLastPoint(e);
-        }
-    };
+            // On délègue la gestion des événements à l'état courant
+            
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    currentState.mousePressed(e);
+                }
 
-			public void mousePressed(MouseEvent e) {
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    currentState.mouseReleased(e);
+                }
+
+                @Override
+                public void mouseDragged(MouseEvent e) {
+                    currentState.mouseDragged(e);
+                }
+
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    currentState.mouseMoved(e);
+                }
+            
+
+        };
+
+        addMouseListener(l);
+        addMouseMotionListener(l);
+        myModel = model;
+    }
+
+
+    /*public void mousePressed(MouseEvent e) {
 				switch (myState) {
 					case INIT:
 						break;
@@ -151,81 +182,78 @@ public class PercentagePieChart extends JComponent implements PercentageView {
 					case ADJUSTING:
 						break;
 				}
-			}
-		};
-		addMouseListener(l);
-		addMouseMotionListener(l);
-		myModel = model;
-	}
+			}*/
+    // "View" behaviour : when the percentage changes, the piechart must be repainted
+    @Override
+    public void notify(PercentageModel model) {
+        repaint();
+    }
 
-	// "View" behaviour : when the percentage changes, the piechart must be repainted
-	public void notify(PercentageModel model) {
-		repaint();
-	}
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int centerX = this.getWidth() / 2;
+        int centerY = this.getHeight() / 2;
+        int radius = Math.min(getWidth() - 4, getHeight() - 4) / 2;
+        double angle = myModel.getValue() * 2 * Math.PI;
+        g.fillOval(centerX - radius, centerY - radius, radius * 2,
+                radius * 2);
+        g.setColor(Color.yellow);
+        g.fillArc(centerX - radius, centerY - radius, radius * 2,
+                radius * 2, 0, (int) Math.toDegrees(angle));
+        int pinX = centerX + (int) (Math.cos(angle) * radius);
+        int pinY = centerY - (int) (Math.sin(angle) * radius);
+        g.setColor(Color.gray.brighter());
+        g.fill3DRect(pinX - 4, pinY - 4, 8, 8, currentState != ADJUSTING);
+    }
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		int centerX = this.getWidth() / 2;
-		int centerY = this.getHeight() / 2;
-		int radius = Math.min(getWidth() - 4, getHeight() - 4) / 2;
-		double angle = myModel.getValue() * 2 * Math.PI;
-		g.fillOval(centerX - radius, centerY - radius, radius * 2,
-			radius * 2);
-		g.setColor(Color.yellow);
-		g.fillArc(centerX - radius, centerY - radius, radius * 2,
-			radius * 2, 0, (int) Math.toDegrees(angle));
-		int pinX = centerX + (int) (Math.cos(angle) * radius);
-		int pinY = centerY - (int) (Math.sin(angle) * radius);
-		g.setColor(Color.gray.brighter());
-		g.fill3DRect(pinX - 4, pinY - 4, 8, 8, myState != PieState.ADJUSTING);
-	}
+    /**
+     * Test if a mouse event is inside the "Pin" that allows to change the
+     * percentage
+     */
+    private boolean inPin(MouseEvent ev) {
+        int mouseX = ev.getX();
+        int mouseY = ev.getY();
+        int centerX = this.getWidth() / 2;
+        int centerY = this.getHeight() / 2;
+        int radius = Math.min(getWidth() - 4, getHeight() - 4) / 2;
+        double angle = myModel.getValue() * 2 * Math.PI;
+        int pinX = centerX + (int) (Math.cos(angle) * radius);
+        int pinY = centerY - (int) (Math.sin(angle) * radius);
 
-	/**
-	 * Test if a mouse event is inside the "Pin" that allows to change the percentage
-	 */
-	private boolean inPin(MouseEvent ev) {
-		int mouseX = ev.getX();
-		int mouseY = ev.getY();
-		int centerX = this.getWidth() / 2;
-		int centerY = this.getHeight() / 2;
-		int radius = Math.min(getWidth() - 4, getHeight() - 4) / 2;
-		double angle = myModel.getValue() * 2 * Math.PI;
-		int pinX = centerX + (int) (Math.cos(angle) * radius);
-		int pinY = centerY - (int) (Math.sin(angle) * radius);
+        Rectangle r = new Rectangle();
+        r.setBounds(pinX - 4, pinY - 4, 8, 8);
+        return r.contains(mouseX, mouseY);
+    }
 
-		Rectangle r = new Rectangle();
-		r.setBounds(pinX - 4, pinY - 4, 8, 8);
-		return r.contains(mouseX, mouseY);
-	}
+    /**
+     * Converts a mouse position to a Percentage value
+     */
+    private float pointToPercentage(MouseEvent e) {
+        int centerX = this.getWidth() / 2;
+        int centerY = this.getHeight() / 2;
+        int mouseX = e.getX() - centerX;
+        int mouseY = e.getY() - centerY;
+        double l = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+        double lx = mouseX / l;
+        double ly = mouseY / l;
+        double theta;
+        if (lx > 0) {
+            theta = Math.atan(ly / lx);
+        } else if (lx < 0) {
+            theta = -1 * Math.atan(ly / lx);
+        } else {
+            theta = 0;
+        }
 
-	/**
-	 * Converts a mouse position to a Percentage value
-	 */
-	private float pointToPercentage(MouseEvent e) {
-		int centerX = this.getWidth() / 2;
-		int centerY = this.getHeight() / 2;
-		int mouseX = e.getX() - centerX;
-		int mouseY = e.getY() - centerY;
-		double l = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
-		double lx = mouseX / l;
-		double ly = mouseY / l;
-		double theta;
-		if (lx > 0) {
-			theta = Math.atan(ly / lx);
-		} else if (lx < 0) {
-			theta = -1 * Math.atan(ly / lx);
-		} else {
-			theta = 0;
-		}
+        if ((mouseX > 0) && (mouseY < 0)) {
+            theta = -1 * theta;
+        } else if (mouseX < 0) {
+            theta += Math.PI;
+        } else {
+            theta = 2 * Math.PI - theta;
+        }
 
-		if ((mouseX > 0) && (mouseY < 0)) {
-			theta = -1 * theta;
-		} else if (mouseX < 0) {
-			theta += Math.PI;
-		} else {
-			theta = 2 * Math.PI - theta;
-		}
-
-		return (float) (theta / (2 * Math.PI));
-	}
+        return (float) (theta / (2 * Math.PI));
+    }
 }
